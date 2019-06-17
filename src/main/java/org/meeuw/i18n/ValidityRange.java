f1fde88d8710b14381f426e6e1e62b42ec385d41
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-06-17 12:12:00 +0200

    Added toString.

--- src/main/java/org/meeuw/i18n/ValidityRange.java
+++ src/main/java/org/meeuw/i18n/ValidityRange.java
@@ -15,6 +15,12 @@ public class ValidityRange {
     public static ValidityRange closed(Year from, Year to) {
         return new ValidityRange(from, to);
     }
+    public static ValidityRange to(Year to) {
+        return new ValidityRange(null, to);
+    }
+    public static ValidityRange from(Year from) {
+        return new ValidityRange(from, null);
+    }
 
     public ValidityRange(Year lowerEndpoint, Year upperEndpoint) {
         this.lowerEndpoint= lowerEndpoint;
@@ -32,6 +38,24 @@ public class ValidityRange {
     public boolean contains(Year year) {
         return (lowerEndpoint == null || ! lowerEndpoint.isAfter(year)) &&
                 (upperEndpoint == null || ! upperEndpoint.isBefore(year));
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        if (lowerEndpoint != null) {
+            builder.append(lowerEndpoint);
+            builder.append(" -");
+        }
+        if (upperEndpoint != null) {
+            if (builder.length() == 0) {
+                builder.append("- ");
+            } else {
+                builder.append(' ');
+            }
+            builder.append(upperEndpoint);
+        }
+        return builder.toString() ;
     }
 }
 
--- src/test/java/org/meeuw/i18n/ValidityRangeTest.java
+++ src/test/java/org/meeuw/i18n/ValidityRangeTest.java
@@ -1,15 +1,14 @@
 package org.meeuw.i18n;
 
-import org.junit.Test;
-
 import java.time.Year;
+
+import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 
 /**
  * @author Michiel Meeuwissen
- * @since ...
  */
 public class ValidityRangeTest {
 
@@ -20,6 +19,17 @@ public class ValidityRangeTest {
 		assertThat(ValidityRange.closed(Year.of(1973), Year.of(2019)).contains(Year.of(2000))).isTrue();
 		assertThat(ValidityRange.closed(Year.of(1973), Year.of(2019)).contains(Year.of(1973))).isTrue();
 		assertThat(ValidityRange.closed(null, Year.of(2019)).contains(Year.of(1973))).isTrue();
+
+	}
+
+	@Test
+	public void testToString() {
+		assertThat(ValidityRange.closed(Year.of(1973), Year.of(2019)).toString()).isEqualTo("1973 - 2019");
+		assertThat(ValidityRange.to(Year.of(2019)).toString()).isEqualTo("- 2019");
+		assertThat(ValidityRange.from(Year.of(1973)).toString()).isEqualTo("1973 -");
+		assertThat(new ValidityRange(null, null).toString()).isEqualTo("");
+
+
 
 	}
 }
