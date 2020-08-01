675ce4031a00e8ef6f1ccaa049a77f8136653c69
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-08-01 11:55:48 +0200

    Upped to junit 5.

--- src/test/java/org/meeuw/i18n/formerlyassigned/ValidityRangeTest.java
+++ src/test/java/org/meeuw/i18n/formerlyassigned/ValidityRangeTest.java
@@ -2,7 +2,7 @@ package org.meeuw.i18n.formerlyassigned;
 
 import java.time.Year;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
